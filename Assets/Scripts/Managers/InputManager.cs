7b78d351885d3cbffb1bbe33df86c3ce6517f8f5
HAPPYURON <ire031226@gmail.com>
2022-09-15 17:23:14 +0900

    Import Input system

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using mDEV.Ui;
 
 
@@ -10,31 +11,37 @@ namespace mDEV.Manager
 {
     public class InputManager : Singleton<InputManager>
     {
+
         public GraphicRaycaster[] raycaster;
 
-        public void UiLButtonDown()
+        private void Start()
         {
-            PointerEventData data = new PointerEventData(null);
-            data.position = Input.mousePosition;
-            List<RaycastResult> results = new List<RaycastResult>();
-            for (int i = 0; i < raycaster.Length; i++)
+             public InputSystem buttonDown;
+    }
+
+    public void UiLButtonDown()
+    {
+        PointerEventData data = new PointerEventData(null);
+        data.position = Input.mousePosition;
+        List<RaycastResult> results = new List<RaycastResult>();
+        for (int i = 0; i < raycaster.Length; i++)
+        {
+            raycaster[i].Raycast(data, results);
+            if (results.Count > 0)
             {
-                raycaster[i].Raycast(data, results);
-                if (results.Count > 0)
+                if (results[0].gameObject.GetComponent<CardUi>())
                 {
-                    if (results[0].gameObject.GetComponent<CardUi>())
-                    {
-                        CardUi tmp = results[0].gameObject.GetComponent<CardUi>();
-                        tmp.transform.position = data.position;
-                    }
-                    break;
+                    CardUi tmp = results[0].gameObject.GetComponent<CardUi>();
+                    tmp.transform.position = data.position;
                 }
+                break;
             }
         }
-
-        private void Update()
-        {
-            UiLButtonDown();
-        }
     }
+
+    private void Update()
+    {
+        UiLButtonDown();
+    }
+}
 }
